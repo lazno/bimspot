6dec084d91eff9123d582fe98fafe05b191b4991
Norbert Lazzeri <lazzeri.norbert@gmail.com>
2019-07-22 13:42:59 +0200

    restructure main method with better printing

--- src/main/kotlin/lazno/bimspot/main.kt
+++ src/main/kotlin/lazno/bimspot/main.kt
@@ -4,21 +4,33 @@ import lazno.bimspot.rest.FuelRedlistClient
 import lazno.bimspot.rest.JacksonDeserializer
 
 val token = "9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee"
+val mammaliaClass = "MAMMALIA"
 
 fun main() {
     val client = FuelRedlistClient(token, JacksonDeserializer)
 
-    val randomRegion = randomEntryOf(client.regions())
+    val regions = client.regions()
+    println("Step 1: fetched ${regions.size} regions")
+    val randomRegion = randomEntryOf(regions)
 
     randomRegion?.let { region ->
-        println("region is $region")
+        println("\nStep 2: random region is $region")
         val species = client.speciesBy(region = region, page = 0)
-                .filter { it.category == Category.CR }
+        println("\nStep 3+4: found ${species.size} species for this region")
 
-        println("found ${species.size} species for this region")
-        species.stream().parallel()
-                .map { spec -> addMeasures(spec, client) }
-                .forEach { println(it) }
+        val endangeredSpecies = species.filter { it.category == Category.CR }
+        println("\nStep 5: found ${endangeredSpecies.size} critically endangered species." )
+        if (endangeredSpecies.isNotEmpty()) {
+            println("fetching conservation measures and printing them")
+        }
+        endangeredSpecies.parallelStream()
+              .map { spec -> addMeasures(spec, client) }
+              .forEach { println(it) }
+
+        val mammals = species.filter { it.class_name == mammaliaClass }
+        println("\nStep 5: printing ${mammals.size} mammals")
+        mammals
+                .forEach{ println(it) }
 
     } ?: run {
         println("region could not be fetched. exiting..")
